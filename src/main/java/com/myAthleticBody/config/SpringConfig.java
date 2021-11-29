dfe32f311a101c8e1a5424c4ed9c51b42e7b3933
VasylBazhan <Rapidvasyaalister@gmail.com>
2021-11-29 20:58:56 +0200

    start build architecture

---
+++ src/main/java/com/myAthleticBody/config/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.myAthleticBody.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+//
+//@Configuration
+//@ComponentScan("com.myAthleticBody")
+//@PropertySource("classpath:app.properties")
+public class SpringConfig {
+}
---
+++ src/main/java/com/myAthleticBody/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.myAthleticBody.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "index";
+    }
+}
