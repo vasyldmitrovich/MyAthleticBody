218e712a5a2c6d502253d893c1b2873d6ec01f6b
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2021-12-03 15:51:12 +0200

    That Controller work with Thymeleaf and return pages

--- src/main/java/com/myAthleticBody/controller/IndexController.java
+++ src/main/java/com/myAthleticBody/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.myAthleticBody.controller;
 
+import com.myAthleticBody.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*In this controller we return pages*/
 @Controller
 public class IndexController {
-    @GetMapping("/index.html")
-    public String sayHello() {
+    @GetMapping()
+    public String indexPage() {
         return "index";
+    }
+
+    @GetMapping("admin/users/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "users/new";
     }
 
 }
